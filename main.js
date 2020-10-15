71eefe442dbcda92dc8ecd2acb29c1e735af0275
Crystal K Young <maybiz@hotmail.com>
2020-10-15 14:57:44 -0400

    almost

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
 // Your code here.
 
+const getFirstName = function(person) {
+  return person.firstName;
+}
 
+const getLastName = function(person) {
+  return person.lastName
+}
+
+const getFullName = function(person) {  
+  return person.firstName + ' ' + person.lastName;
+}
+
+const setFirstName = function(person, fName) {
+  return person.firstName = fName 
+}
+
+const setAge = function(person, age) {
+  return person.age = age
+}
+
+const giveBirthday = function(person) {
+  if (person.age > 0) {
+    return person.age = person.age + 1
+  } else { 
+    return person.age = 1
+  }
+}
+
+const marry = function(person1, person2) {
+  if (person1.married  === false && person2.married === false) {
+    person1.married = true
+    person2.married = true
+  } 
+  person1.spouseName = person2.firstName + ' ' + person2.lastName;
+  person2.spouseName = person1.firstName + ' ' + person1.lastName;
+}
+
+
+const divorce = function(person1, person2) {
+    person1.married = false 
+    person2.married = false
+
+//     delete person1[married]
+//     delete person2[married]
+}
 
 
 
